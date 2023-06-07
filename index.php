29e02b222e5fe6f97dead2b98de1029fdc8fc007
bovtenkohub <135398423+bovtenkohub@users.noreply.github.com>
2023-06-08 01:30:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -28,12 +28,76 @@
             <button type="submit">Отправить заметку</button>
         </form>
     </div>
-    <div id="board" style="width: 400px; margin: 0 auto;"></div>
+   <style>
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+#board {
+    width: 500px;
+    height: 500px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.square {
+    width: 60px;
+    height: 60px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+}
+.white {
+    background-color: #E9ECF0;
+}
+.black {
+    background-color: #B5BDCE;
+}
+.wr { background-image: url('/Users/evgenijboltenko/Desktop/project/a109wr.png'); }
+.wn { background-image: url('/Users/evgenijboltenko/Desktop/project/a109wn.png'); }
+.wb { background-image: url('/Users/evgenijboltenko/Desktop/project/a109wb.png'); }
+.wq { background-image: url('/Users/evgenijboltenko/Desktop/project/a109wq.png'); }
+.wk { background-image: url('/Users/evgenijboltenko/Desktop/project/a109wk.png'); }
+.wp { background-image: url('/Users/evgenijboltenko/Desktop/project/a109wp.png'); }
+.br { background-image: url('/Users/evgenijboltenko/Desktop/project/a109br.png'); }
+.bn { background-image: url('/Users/evgenijboltenko/Desktop/project/a109bn.png'); }
+.bb { background-image: url('/Users/evgenijboltenko/Desktop/project/a109bb.png'); }
+.bq { background-image: url('/Users/evgenijboltenko/Desktop/project/a109bq.png'); }
+.bk { background-image: url('/Users/evgenijboltenko/Desktop/project/a109bk.png'); }
+.bp { background-image: url('/Users/evgenijboltenko/Desktop/project/a109bp.png'); }
+</style>
 
-    <textarea id="fen" rows="1" style="width: 100%; max-width: 400px; margin: 0 auto; display: block;"></textarea>
-    <button id="play">Play</button>
+<body>
+<div id="board">
+    <!-- В этом месте будет создана шахматная доска -->
+</div>
 
-    <script src="chessScript.js"></script>
+<script>
+var board = document.getElementById('board');
+var pieces = [
+    'br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br',
+    'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp',
+    'wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'
+];
+for (var i = 0; i < 64; i++) {
+    var square = document.createElement('div');
+    square.className = 'square ' + ((i + Math.floor(i/8)) % 2 === 0 ? 'white ' : 'black ') + pieces[i];
+    board.appendChild(square);
+}
+</script>
+
+<script src="chess.js"></script>
+
+
+</body>
+
 </body>
 </html>
 
